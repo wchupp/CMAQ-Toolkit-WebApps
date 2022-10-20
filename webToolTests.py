350f1d4e51d8225b91c235f16bc0cb85d8420687
wchupp <william.chupp@dot.gov>
2022-10-20 16:39:20 -0400

    Initial Commit

---
+++ webToolTests.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 14:52:47 2022
+
+@author: william.chupp
+"""
+
+# importing modules
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.image('CMAQToolkitLogo.png')
+st.title('CMAQ Toolkit Teleworking Tool')
+
+
+
+#  This is the pywebio hello-world
+
+# # classify person
+# class calculation:
+
+# 	# defining method
+# 	def BMIcalculator(Height, Mass):
+
+# 		for t1, t2 in [(16, 'severely underweight'),
+# 					(18.5, 'underweight'),
+# 					(25, 'normal'),
+# 					(30, 'overweight'),
+# 					(35, 'moderately obese'),
+# 					(float('inf'), 'severely obese')]:
+# 			if BMI <= t1:
+# 				put_text('Your BMI is', BMI, 'and the person is :', t2)
+# 				break
+
+# # classify and compute BMI
+# class calculation:
+
+# 	# defining method
+# 	def BMIcalculator(self, Height, Mass):
+
+# 		# compute BMI
+# 		BMI = (Mass)/(Height*Height)
+
+# 		# classify
+# 		for t1, t2 in [(16, 'severely underweight'),
+# 					(18.5, 'underweight'),
+# 					(25, 'normal'), (30, 'overweight'),
+# 					(35, 'moderately obese'),
+# 					(float('inf'), 'severely obese')]:
+
+# 			if BMI <= t1:
+# 				put_text('Your BMI is', BMI, 'and the person is :', t2)
+# 				break
+
+
+# # height input
+# Height = input("Please enter height in meters(m)", type=FLOAT)
+
+# # Mass input
+# Mass = input("Please enter Mass/Weight in Kilograms(Kg)", type=FLOAT)
+
+# obj = calculation()
+# obj.BMIcalculator(Height, Mass)
+
